8f986bdf1cf2bc02fd0f9af9d6fd3b624f94ae93
Hackercodeß <130597868+MohammedFarouq@users.noreply.github.com>
2023-04-23 07:33:33 +0100

    Update File.py

--- File.py
+++ File.py
@@ -12,7 +12,7 @@ try:
  import requests
  from concurrent.futures import ThreadPoolExecutor as ThreadPool
  import mechanize
-from requests.exceptions import ConnectionError
+import ConnectionError
 except ModuleNotFoundError:
     os.system('pip install mechanize requests futures bs4==2 > /dev/null')
     os.system('pip install bs4')
